280f37dc94ab33f9409711c49bcf781a614fe5ea
Nataliia Lyfar <lifarnatala@gmail.com>
2022-07-21 23:25:16 +0300

    fixed section padding-bottom

--- src/components/ui/Section.js
+++ src/components/ui/Section.js
@@ -2,8 +2,6 @@ import styled from "styled-components";
 import { breakpoints } from "styleConfig/breakpoints";
 
 export const Section = styled.section`
-padding: ${p => p.theme.space[6]}px 0 ${p => p.theme.space[7]}px;
-@media ${breakpoints.tablet} {
-    padding: ${p => p.theme.space[6]}px 0 ${p => p.theme.space[8]}px;  
-};
+padding: ${p => p.theme.space[6]}px 0 ${p => p.theme.space[5]}px;
+
 `;
