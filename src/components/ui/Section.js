6c84866d9168988ea940c7f666ce49a236b8b7bb
Nataliia Lyfar <lifarnatala@gmail.com>
2022-07-21 23:26:12 +0300

    edited breakpoints

--- src/components/ui/Section.js
+++ src/components/ui/Section.js
@@ -1,7 +1,5 @@
 import styled from "styled-components";
-import { breakpoints } from "styleConfig/breakpoints";
 
 export const Section = styled.section`
 padding: ${p => p.theme.space[6]}px 0 ${p => p.theme.space[5]}px;
-
 `;
